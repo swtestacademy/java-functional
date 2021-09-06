54381fde4082dec0a2d31576df8c0365a890f213
Onur Baskirt <onur.baskirt@dnata.com>
2021-09-06 10:46:48 +0400

    text blocks examples added.

---
+++ src/test/java/java12to17/textblocks/TextBlocks.java
@@ -0,0 +1,46 @@
+package java12to17.textblocks;
+
+import org.junit.jupiter.api.Test;
+
+public class TextBlocks {
+
+    /**
+     * A text block is opened by three double quotes """ followed by a line break, and closed by three double-quotes.
+     */
+    @Test
+    public void textBlocksTest() {
+        String textBlockFootballers = """
+            Footballers
+              with double space indentation
+                and "SW TEST ACADEMY TEAM" Rocks!
+            """;
+        System.out.println(textBlockFootballers);
+    }
+
+    /**
+     * Trailing white space is removed from every line, which can be prevented by using the new escape sequence \s.
+     */
+    @Test
+    public void textBlocksNoLineBreaksTest() {
+        String textBlockFootballers = """
+            Footballers \
+            with double space indentation \
+            and "SW TEST ACADEMY TEAM" Rocks! \
+            """;
+        System.out.println(textBlockFootballers);
+    }
+
+    /**
+     * Inserting variables into a text block can be done as usual with the static method String::format,
+     * or with the new instance method String::formatted
+     */
+    @Test
+    public void textBlocksInsertingVariablesTest() {
+        String textBlockFootballers = """
+            Footballers
+              with double space indentation
+                and "%s" Rocks!
+            """.formatted("SW TEST ACADEMY TEAM");
+        System.out.println(textBlockFootballers);
+    }
+}
