593623fe72703ca52ee7b2616bceb026a5a83c9e
Onur Baskirt <onur.baskirt@dnata.com>
2021-09-15 22:25:08 +0400

    Car Test updated.

--- src/test/java/basics/polymorphism/inheritancepolymorphism/CarTest.java
+++ src/test/java/basics/polymorphism/inheritancepolymorphism/CarTest.java
@@ -3,24 +3,24 @@ package basics.polymorphism.inheritancepolymorphism;
 import org.junit.jupiter.api.Test;
 
 public class CarTest {
-    Car tesla    = new Tesla();
-    Car porsche  = new Porsche();
-    Car mercedes = new Mercedes();
+    Car tesla    = new Tesla(); //You can also declare like this: Tesla tesla = new Tesla();
+    Car porsche  = new Porsche(); //You can also declare like this: Porsche porsche = new Porsche()
+    Car mercedes = new Mercedes(); //You can also declare like this: Mercedes mercedes = new Mercedes();
 
     @Test
     public void cartTest() {
         tesla.setName("Tesla");
-        System.out.println("\nTesting " +  tesla.getName() + "!\n-------------------");
+        System.out.println("\nTesting " + tesla.getName() + "!\n-------------------");
         tesla.speedUp();
         tesla.consumeEnergy();
 
         porsche.setName("Porsche");
-        System.out.println("\nTesting " +  porsche.getName() + "!\n-------------------");
+        System.out.println("\nTesting " + porsche.getName() + "!\n-------------------");
         porsche.speedUp();
         porsche.consumeEnergy();
 
         mercedes.setName("Mercedes");
-        System.out.println("\nTesting " +  mercedes.getName() + "!\n-------------------");
+        System.out.println("\nTesting " + mercedes.getName() + "!\n-------------------");
         mercedes.speedUp();
         mercedes.consumeEnergy();
     }
