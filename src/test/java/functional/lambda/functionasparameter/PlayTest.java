9942d63389062f13a67a8e4ca5e2b228e2268a0e
Onur Baskirt <onur.baskirt@dnata.com>
2021-09-16 22:38:03 +0400

    Minor fixes.

--- src/test/java/functional/lambda/functionasparameter/PlayTest.java
+++ src/test/java/functional/lambda/functionasparameter/PlayTest.java
@@ -6,15 +6,15 @@ public class PlayTest {
         PlayService playIt = (name, duration, repeat) ->
             System.out.println(name.toUpperCase() + " is playing for " + duration + " minutes." + " Repeat is: " + repeat + "\n");
 
-        //Function calling by sending the function as a parameter.
+        //Function calling by sending the function as an argument.
         testPlay(playIt);
 
-        //Function calling by sending the function as a parameter as lambda declaration.
+        //Function calling by sending the function as an argument as lambda declaration.
         testPlay((name, duration, repeat) ->
             System.out.println(name.toLowerCase() + " is playing for " + duration + " minutes." + " Repeat is: " + repeat + "\n"));
     }
 
-    //Function as a parameter.
+    //Declaring function as a parameter.
     private static void testPlay(PlayService playService) {
         System.out.println("This is a song play test!");
         playService.run("Despacito", 3.20, true);
