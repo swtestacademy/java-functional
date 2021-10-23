1c540572bd734e67bd3f419532cb62e4128b9aa6
Onur Baskirt <onur.baskirt@dnata.com>
2021-10-23 18:16:59 +0400

    HTTP Connection Util update. Timeout added.

--- src/test/java/functional/stream/examples/brokenlinks/HTTPConnectionUtil.java
+++ src/test/java/functional/stream/examples/brokenlinks/HTTPConnectionUtil.java
@@ -7,6 +7,8 @@ import lombok.SneakyThrows;
 public class HTTPConnectionUtil {
     @SneakyThrows
     public static int getResponseCode(String address) {
-        return ((HttpURLConnection) (new URL(address)).openConnection()).getResponseCode();
+        HttpURLConnection httpURLConnection = (HttpURLConnection) new URL(address).openConnection();
+        httpURLConnection.setConnectTimeout(8000);
+        return httpURLConnection.getResponseCode();
     }
 }
